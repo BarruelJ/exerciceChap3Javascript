eaae8d18df72c03f9d9a4ab3ed2419e3889202df
Joan Barruel <j.barruel@icloud.com>
2025-12-15 17:36:22 +0100

    Mise en commentaire de la commande LancerJeu() pour éviter les pop-up (à réactiver), ciblage de différent éléments dans main.js pour l'exercice chap3

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,19 @@
  * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
  * 
  *********************************************************************************/
+let texteUtilisateur = document.getElementById("inputEcriture")
+console.log(texteUtilisateur)
 
-lancerJeu()
+let boutonValide = document.getElementById("btnValiderMot")
+console.log(boutonValide)
+
+let motProposer = document.querySelector(".zoneProposition")
+console.log(motProposer)
+
+let afficheScore = document.querySelector(".zoneScore span")
+console.log(afficheScore)
+
+let boutonChoix = document.querySelectorAll(".optionSource input")
+console.log(boutonChoix)
+
+// lancerJeu()
